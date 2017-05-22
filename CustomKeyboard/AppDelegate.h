cd1b4de346636dbe7d2daa7f9af3e22444c688f4
BurtKing <297975451@qq.com>
2017-05-22 16:21:31 +0800

    Initial Commit

---
+++ CustomKeyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomKeyboard
+//
+//  Created by Burt on 2017/5/22.
+//  Copyright © 2017年 com.uqiauto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomKeyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomKeyboard
+//
+//  Created by Burt on 2017/5/22.
+//  Copyright © 2017年 com.uqiauto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
